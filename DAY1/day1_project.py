f2bee0818946f1165a4d7a4ae05d6d337f5621fb
shiwanibiradar <shiwanib1099@gmail.com>
2026-01-05 01:15:08 +0530

    Day1 directory

---
+++ DAY1/day1_project.py
@@ -0,0 +1,5 @@
+print("Welcome to brand name generate")
+a=input("What's the name of the city you grew up in?")
+b=input("whats your pet name")
+
+print("Your brand name could be"+" "+a +b)
---
+++ DAY1/inputstring.py
@@ -0,0 +1,7 @@
+print("hello" + " " + input("what is your name?") + "!")
+
+name1=input("what is your first name")
+name2=input("what is your second name")
+add=len(name1)+len(name2)
+print(add)
+# print(len(name)+len(name2))
